70654fc7ac8f844674cddd3925737f054d679910
Wellington Camargo Dias <150048399+WellingtonCamargoDias0911@users.noreply.github.com>
2024-03-28 13:40:28 -0300

    refactor.alter RoboBinance.py

--- RoboBinance.py
+++ RoboBinance.py
@@ -1,5 +1,9 @@
 import requests
 import json
+import matplotlib.pyplot as plt
+
+moeda = "BTC"
+intervalo = "1m"
 
 def pegaCotacoes(moeda, periodoMinutos, diasPassados):
     codigoMoeda = moeda + 'USDT'
@@ -8,10 +12,26 @@ def pegaCotacoes(moeda, periodoMinutos, diasPassados):
 
     req = requests.get(url)
     dados = json.loads(req.text)
+    return dados
 
+while True:
+    dados = pegaCotacoes(moeda, intervalo, None)
     cotacoes = []
+    lista_media_rapida = []
+    lista_media_lenta = []
     for d in dados:
-        cotacoes.append({"close":d[4]})
-    return cotacoes
+        cotacoes.append(float(d[4]))
+        mediaRapida = sum(cotacoes[-8:]) / 8
+        mediaLenta = sum(cotacoes[-21:]) / 21
+        lista_media_rapida.append(mediaRapida)
+        lista_media_lenta.append(mediaLenta)
 
-print(pegaCotacoes("BTC", "15m", None))
+
+    ag = plt.gca()
+    ag.clear()
+    plt.plot(cotacoes[-50:])
+    plt.plot(lista_media_rapida[-50:], color="red")
+    plt.plot(lista_media_lenta[-50:], color="green")
+    plt.draw()
+
+    plt.pause(5)
